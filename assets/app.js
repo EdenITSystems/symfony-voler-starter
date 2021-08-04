067a250e8a6f7f55a163e3bc7c2133d375623413
maesterlyb <yves.ekanga27@gmail.com>
2021-08-04 11:24:59 +0100

    [feat]: initialisation et integration du template

---
+++ assets/app.js
@@ -0,0 +1,24 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+// any CSS you import will output into a single css file (app.css in this case)
+import './styles/app.scss';
+
+// you can specify whith plugins you need
+import { Tooltip, Toast, Popover } from 'bootstrap';
+
+import './js/app'
+import './js/feather-icons'
+import './js/main'
+import './js/feather-icons/feather'
+import './js/pages/dashboard'
+import './vendors/chartjs/Chart'
+import './vendors/apexcharts/apexcharts'
+
+
+// start the Stimulus application
+import './bootstrap';
